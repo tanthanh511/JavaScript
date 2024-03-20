67e3cb573738e9f47ac52de26556b5f2de1b2c70
Huỳnh Tấn Thanh <117253637+tanthanh511@users.noreply.github.com>
2024-03-20 09:30:52 +0700

    class

--- du-an-test/app.js
+++ du-an-test/app.js
@@ -1,17 +1,74 @@
+
 var courseApi ="http://localhost:3000/course"
-fetch(courseApi)
-    .then((response)=> response.json())
-    .then(function(courses){
-      console.log(courses);
-      var courseBlock = document.getElementById('course-box')
-      console.log(courseBlock);
-      var html= ''
-      courses.forEach(function(course) {
-         html += `
+
+function start(){
+   getCourses(renderCourses);
+   handleCreateForm();
+}
+
+start();
+
+function getCourses(callback){
+   fetch(courseApi)
+      .then(function(response){
+         return response.json()
+      })
+      .then(callback);
+}
+
+function renderCourses(courses){
+   console.log(courses);
+   var courseBox = document.getElementById('course-box')
+   var html = courses.map(function(course){
+      return `
+         <li>
             <h2>${course.name}</h2>
-            <p>${course.description}</p>`
-         
-      });
-      courseBlock.innerHTML= html
-    })
+            <p>${course.description}</p>
+            <button onclick="deleteCourse(${course.id})">&times;</button>
+         </li>
+      `
+   });
+   courseBox.innerHTML = html.join('')
+}
+
+function handleCreateForm(){
+   var createBtn = document.getElementById('create')
+   createBtn.onclick = function(){
+      var name = document.querySelector('input[name="name"]').value
+      var description = document.querySelector('input[name="description"]').value
+      var formData= {
+         name : name,
+         description: description
+      }
+      createCourse(formData)
+   }
+}
+
+function createCourse(data, callback){
+   var option = {
+      method: "POST",
+      body: JSON.stringify(data)
+   }
+   fetch(courseApi, option)
+      .then(function(response){
+         response.json();
+      })
+      .then(callback)
+}
+
+function handleDeleteCourses(){
+   var option = {
+      method: "DELETE"
+   }
+   fetch(courseApi + '/'+ id, option)
+      .then(function(response){
+         response.json();
+      })
+      .then(function(){
+         getCourses(renderCourses)
+      })
+}
+
+
+
  
